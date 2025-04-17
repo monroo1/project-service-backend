9d44107c6a0d02d56c8453a6d528bafdd9f083a1
monroo <andrey.monroo@gmail.com>
2025-04-17 12:16:57 +0300

    feat: change host

--- config/server.ts
+++ config/server.ts
@@ -7,5 +7,5 @@ export default ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
-  url: "https://xn--b1afanhwgieieg.online/strapi/",
+  url: "https://proekt-service.online/strapi/",
 });
